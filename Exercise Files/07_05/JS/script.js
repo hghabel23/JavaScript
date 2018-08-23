d9b0efbf0c80e6e3805001369cbf407158aa4957
hghabel <hghabel@nextgen.com>
2018-08-23 09:34:57 -0400

    change button text when click

--- Exercise Files/07_05/JS/script.js
+++ Exercise Files/07_05/JS/script.js
@@ -5,11 +5,15 @@ CTA.classList.remove("hide");
 ALERT.classList.add("hide");
 
 /* we want to change button text when it is clicked */
-function reveal(e) {
+//current is the clicked object which is CTA here.
+function reveal(e, current) {
     e.preventDefault();
-    CTA.classList.toggle("hide");
+    
+	console.log(current.innerHTML);
+	current.innerHTML == "Book Now!" ? CTA.innerHTML = "Ooooops!" : CTA.innerHTML = "Book Now!";
+	
     ALERT.classList.toggle("hide");
 }
 
-CTA.addEventListener('click', reveal, false);
+CTA.addEventListener('click', function(e){reveal(e, this);}, false);
 CTA.addEventListener('click', function(){console.log("The button was clicked!")}, false);
