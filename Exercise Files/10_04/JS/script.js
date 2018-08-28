2a7b200621564c5521faa60b21cd6c8b5c627403
hghabel <hghabel@nextgen.com>
2018-08-28 13:47:19 -0400

    get images with different width in a loop

--- Exercise Files/10_04/JS/script.js
+++ Exercise Files/10_04/JS/script.js
@@ -1,9 +1,23 @@
 const IMAGES = document.querySelectorAll("img");
 
+function makeSrcSet(imgSrc){
+	let markup = [];
+	let width = 400; //smalles size for our images
+	
+	for (let i = 0; i <5; i++){
+		markup[i] = imgSrc + "-" + width + ".jpg " + width + "w";
+		width += 400;
+	}
+	
+	return markup.join(); //join will put all the items in a string with comma seperator
+	
+}
+
 for (let i = 0; i<IMAGES.length; i++) {
     let imgSrc = IMAGES[i].getAttribute("src");
     imgSrc = imgSrc.slice(0,-8);
-    console.log(imgSrc);
+	let srcset = makeSrcSet(imgSrc);
+    console.log(srcset);
 
     let type = IMAGES[i].getAttribute("data-type");
     console.log(type);
