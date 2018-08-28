1498029201c9715fe4b9bb4e5d661a50144ffbd1
hghabel <hghabel@nextgen.com>
2018-08-28 10:25:04 -0400

    For-Loops

--- Exercise Files/09_02/JS/script.js
+++ Exercise Files/09_02/JS/script.js
@@ -1,1 +1,9 @@
+// the ^ means any object that has the href attribute which starts with the value "http".
+var extLinks = document.querySelectorAll('a[href^="http"]');
 
+for (var i = 0; i < extLinks.length; i++){
+	console.log(extLinks[i]);
+	if (!extLinks[i].hasAttribute("target")){
+		extLinks[i].setAttribute("target","_blank");
+	}
+}
