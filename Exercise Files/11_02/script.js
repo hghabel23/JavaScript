df6be7a669dc55c0d64f93206ff9ccf73cd815ed
hghabel <hghabel@nextgen.com>
2018-09-19 10:07:42 -0400

    11-02 Debugging and console.log

--- Exercise Files/11_02/script.js
+++ Exercise Files/11_02/script.js
@@ -20,7 +20,7 @@ function leadingZero(time) {
 function runTimer() {
     let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
-    timer[3]+;
+    timer[3]++;
 
     timer[0] = Math.floor((timer[3]/100)/60);
     timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
@@ -31,9 +31,11 @@ function runTimer() {
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
+	console.log("Text Entered: ", textEntered);
     let originTextMatch = originText.substring(0,textEntered.length);
 
     if (textEntered == originText) {
+		console.info("interval stopped: ", interval);
         clearInterval(interval);
         testWrapper.style.borderColor = "#429890";
     } else {
@@ -52,6 +54,7 @@ function start() {
     if (textEnterdLength === 0 && !timerRunning) {
         timerRunning = true;
         interval = setInterval(runTimer, 10);
+		console.info(interval);
     }
 }
 
